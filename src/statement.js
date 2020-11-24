8a9c8086493a9005bcafc6ad877bf24d99a58ca4
AlbinoFreitas <albinofreitas7@gmail.com>
2020-11-24 19:02:51 -0300

    extrat função - amountFor

--- src/statement.js
+++ src/statement.js
@@ -9,7 +9,22 @@ function statement(invoice, plays) {
     }).format;
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
+    let thisAmount = amountFor(perf, play);
+    // soma créditos por volume
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    // soma um crédito extra para cada dez espectadores de comédia
+    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    // exibe a linha para esta requisição
+    result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
+    totalAmount += thisAmount;
+  }
+  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `You earned ${volumeCredits} credits\n`;
+  return result;
+
+  function amountFor(perf, play) {
     let thisAmount = 0;
+
     switch (play.type) {
       case "tragedy":
         thisAmount = 40000;
@@ -26,17 +41,9 @@ function statement(invoice, plays) {
       default:
         throw new Error(`unknown type: ${play.type}`);
     }
-    // soma créditos por volume
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // soma um crédito extra para cada dez espectadores de comédia
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-    // exibe a linha para esta requisição
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
-    totalAmount += thisAmount;
+
+    return thisAmount;
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
-  return result;
 }
 
 module.exports = { statement };
