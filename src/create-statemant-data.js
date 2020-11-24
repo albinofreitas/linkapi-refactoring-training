85e493d4a495616c62e3d801d0a61a307515c6cd
AlbinoFreitas <albinofreitas7@gmail.com>
2020-11-24 20:13:55 -0300

    mover amoutFor para calculator

--- src/create-statemant-data.js
+++ src/create-statemant-data.js
@@ -19,7 +19,7 @@ module.exports = function createStatementData(invoice, plays) {
       play: calculator.play
     };
 
-    result.amount = amountFor(result);
+    result.amount = calculator.amount;
     result.volumeCredits = volumeCreditsFor(result);
 
     return result;
@@ -38,29 +38,6 @@ module.exports = function createStatementData(invoice, plays) {
 
     for (let perf of data.performances) {
       result += perf.volumeCredits;
-    }
-
-    return result;
-  }
-
-  function amountFor(performance) {
-    let result = 0;
-
-    switch (performance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (performance.audience > 30) {
-          result += 1000 * (performance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000; if (performance.audience > 20) {
-          result += 10000 + 500 * (performance.audience - 20);
-        }
-        result += 300 * performance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${performance.play.type}`);
     }
 
     return result;
